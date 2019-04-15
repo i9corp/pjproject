79e0f9aae9c97b04eee8a729a21cf0c0c66d179d
Sileno O Brito <silenobrito@gmail.com>
2019-04-15 17:34:32 -0300

    Alterado comando make

--- extras/deploy.sh
+++ extras/deploy.sh
@@ -10,14 +10,16 @@ PATH=/usr/bin:$PATH
 
 export PATH="${PATH}"
 MINGW_HOME="C:/msys32/mingw32"
+MINGW_BIN="${MINGW_HOME}/bin"
+COMMAND_MAKE="${MINGW_BIN}/mingw32-make.exe"
 PACKAGE_DIR="${PROJECT_DIR}\dist"
 function build(){
     cd ${PROJECT_DIR}
 
     ./configure --disable-speex-aec --disable-l16-codec --disable-speex-codec --disable-opencore-amr  --disable-v4l2 --prefix=${PACKAGE_DIR}
-    make dep
-    make
-    make install
+    ${COMMAND_MAKE} dep
+    ${COMMAND_MAKE}
+    ${COMMAND_MAKE} install
 }
 
 build
