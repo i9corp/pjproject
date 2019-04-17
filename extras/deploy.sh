6fbb7648d43b7256579a3c8e0ac44e7bd830b709
I9CORP LTDA ME <37485088+i9corp@users.noreply.github.com>
2019-04-17 18:32:29 -0300

    Update deploy.sh

--- extras/deploy.sh
+++ extras/deploy.sh
@@ -16,7 +16,7 @@ PACKAGE_DIR="${PROJECT_DIR}\dist"
 function build(){
     cd ${PROJECT_DIR}
 
-    ./configure --disable-speex-aec --disable-l16-codec --disable-speex-codec --disable-opencore-amr  --disable-v4l2 --prefix=${PACKAGE_DIR}
+    ./configure --disable-video --disable-speex-aec --disable-l16-codec --disable-speex-codec --disable-sdl --disable-openh264 --disable-v4l2 --disable-opus --disable-opencore-amr --disable-ssl  --disable-libyuv --disable-libwebrtc --disable-bcg729 --disable-sound --disable-resample --disable-shared --prefix=${PACKAGE_DIR}
     ${COMMAND_MAKE} dep
     ${COMMAND_MAKE}
     ${COMMAND_MAKE} install
